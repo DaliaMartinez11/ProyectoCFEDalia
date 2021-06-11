49b19c38b3e39ecb62724a7a3734f0a9fe4167f9
FlutLab.io <>
2021-06-11 08:31:18 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedcomponent4widget/GeneratedComponent4Widget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedloginwidget/GeneratedLoginWidget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedbotonwidget1/GeneratedBOTONWidget1.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedbotonwidget/GeneratedBotonWidget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedsettingssharpwidget/GeneratedSettingssharpWidget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatediniciowidget/GeneratedInicioWidget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedcrearpropiedadwidget/GeneratedCrearpropiedadWidget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedfavoritoswidget/GeneratedFavoritosWidget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedperfilwidget6/GeneratedPerfilWidget6.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedcomponent1widget/GeneratedComponent1Widget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedframe1widget/GeneratedFrame1Widget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedajusteswidget10/GeneratedAjustesWidget10.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedloginwidget1/GeneratedLoginWidget1.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedsettingssharpwidget1/GeneratedSettingssharpWidget1.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatediniciowidget7/GeneratedInicioWidget7.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedcrearpropiedadwidget1/GeneratedCrearpropiedadWidget1.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedfavoritoswidget1/GeneratedFavoritosWidget1.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedperfilwidget19/GeneratedPerfilWidget19.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedframe2widget/GeneratedFrame2Widget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedajusteswidget24/GeneratedAjustesWidget24.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedloginwidget2/GeneratedLoginWidget2.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedsettingssharpwidget2/GeneratedSettingssharpWidget2.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatediniciowidget14/GeneratedInicioWidget14.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedcrearpropiedadwidget2/GeneratedCrearpropiedadWidget2.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedfavoritoswidget2/GeneratedFavoritosWidget2.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedperfilwidget32/GeneratedPerfilWidget32.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedmenuinferiorwidget16/GeneratedMenuinferiorWidget16.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedmenusuperiorwidget13/GeneratedMenusuperiorWidget13.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedajusteswidget38/GeneratedAjustesWidget38.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedloginwidget3/GeneratedLoginWidget3.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedsettingssharpwidget3/GeneratedSettingssharpWidget3.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatediniciowidget21/GeneratedInicioWidget21.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedcrearpropiedadwidget3/GeneratedCrearpropiedadWidget3.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedfavoritoswidget3/GeneratedFavoritosWidget3.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedperfilwidget45/GeneratedPerfilWidget45.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedmenuinferiorwidget22/GeneratedMenuinferiorWidget22.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedmenusuperiorwidget18/GeneratedMenusuperiorWidget18.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedajusteswidget52/GeneratedAjustesWidget52.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedcomponent3widget/GeneratedComponent3Widget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedmenuinferiorwidget24/GeneratedMenuinferiorWidget24.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedinputwidget8/GeneratedInputWidget8.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedbotonwidget5/GeneratedBotonWidget5.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedbuscadorwidget4/GeneratedBuscadorWidget4.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedmenusuperiorwidget20/GeneratedMenusuperiorWidget20.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedcfe21widget4/GeneratedCFE21Widget4.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedloginwidget4/GeneratedLoginWidget4.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedbotonwidget6/GeneratedBOTONWidget6.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedbotonwidget6/GeneratedBotonWidget6.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedsettingssharpwidget4/GeneratedSettingssharpWidget4.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatediniciowidget29/GeneratedInicioWidget29.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedcrearpropiedadwidget4/GeneratedCrearpropiedadWidget4.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedfavoritoswidget4/GeneratedFavoritosWidget4.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedperfilwidget60/GeneratedPerfilWidget60.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedcomponent4widget4/GeneratedComponent4Widget4.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedframe3widget/GeneratedFrame3Widget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedajusteswidget68/GeneratedAjustesWidget68.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedloginwidget5/GeneratedLoginWidget5.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedsettingssharpwidget5/GeneratedSettingssharpWidget5.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatediniciowidget36/GeneratedInicioWidget36.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedcrearpropiedadwidget5/GeneratedCrearpropiedadWidget5.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedfavoritoswidget5/GeneratedFavoritosWidget5.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedperfilwidget73/GeneratedPerfilWidget73.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedframe4widget/GeneratedFrame4Widget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedajusteswidget82/GeneratedAjustesWidget82.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedloginwidget6/GeneratedLoginWidget6.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedsettingssharpwidget6/GeneratedSettingssharpWidget6.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatediniciowidget43/GeneratedInicioWidget43.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedcrearpropiedadwidget6/GeneratedCrearpropiedadWidget6.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedfavoritoswidget6/GeneratedFavoritosWidget6.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatednoticia21widget/GeneratedNoticia21Widget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatednoticia31widget/GeneratedNoticia31Widget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatednoticia11widget/GeneratedNoticia11Widget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedperfilwidget86/GeneratedPerfilWidget86.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedmenuinferiorwidget41/GeneratedMenuinferiorWidget41.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedmenusuperiorwidget34/GeneratedMenusuperiorWidget34.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedajusteswidget96/GeneratedAjustesWidget96.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedcomponent5widget/GeneratedComponent5Widget.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedinputwidget15/GeneratedInputWidget15.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedbotonwidget10/GeneratedBotonWidget10.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedbuscadorwidget8/GeneratedBuscadorWidget8.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedmenusuperiorwidget36/GeneratedMenusuperiorWidget36.dart';
+import 'package:flutterapp/proyecto_20figma_20cfeapp/generatedcfe22widget7/GeneratedCFE22Widget7.dart';
+
+void main() {
+  runApp(Proyecto_20Figma_20CFEApp());
+}
+
+class Proyecto_20Figma_20CFEApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedLoginWidget',
+      routes: {
+        '/GeneratedComponent4Widget': (context) => GeneratedComponent4Widget(),
+        '/GeneratedLoginWidget': (context) => GeneratedLoginWidget(),
+        '/GeneratedBOTONWidget1': (context) => GeneratedBOTONWidget1(),
+        '/GeneratedBotonWidget': (context) => GeneratedBotonWidget(),
+        '/GeneratedSettingssharpWidget': (context) =>
+            GeneratedSettingssharpWidget(),
+        '/GeneratedInicioWidget': (context) => GeneratedInicioWidget(),
+        '/GeneratedCrearpropiedadWidget': (context) =>
+            GeneratedCrearpropiedadWidget(),
+        '/GeneratedFavoritosWidget': (context) => GeneratedFavoritosWidget(),
+        '/GeneratedPerfilWidget6': (context) => GeneratedPerfilWidget6(),
+        '/GeneratedComponent1Widget': (context) => GeneratedComponent1Widget(),
+        '/GeneratedFrame1Widget': (context) => GeneratedFrame1Widget(),
+        '/GeneratedAjustesWidget10': (context) => GeneratedAjustesWidget10(),
+        '/GeneratedLoginWidget1': (context) => GeneratedLoginWidget1(),
+        '/GeneratedSettingssharpWidget1': (context) =>
+            GeneratedSettingssharpWidget1(),
+        '/GeneratedInicioWidget7': (context) => GeneratedInicioWidget7(),
+        '/GeneratedCrearpropiedadWidget1': (context) =>
+            GeneratedCrearpropiedadWidget1(),
+        '/GeneratedFavoritosWidget1': (context) => GeneratedFavoritosWidget1(),
+        '/GeneratedPerfilWidget19': (context) => GeneratedPerfilWidget19(),
+        '/GeneratedFrame2Widget': (context) => GeneratedFrame2Widget(),
+        '/GeneratedAjustesWidget24': (context) => GeneratedAjustesWidget24(),
+        '/GeneratedLoginWidget2': (context) => GeneratedLoginWidget2(),
+        '/GeneratedSettingssharpWidget2': (context) =>
+            GeneratedSettingssharpWidget2(),
+        '/GeneratedInicioWidget14': (context) => GeneratedInicioWidget14(),
+        '/GeneratedCrearpropiedadWidget2': (context) =>
+            GeneratedCrearpropiedadWidget2(),
+        '/GeneratedFavoritosWidget2': (context) => GeneratedFavoritosWidget2(),
+        '/GeneratedPerfilWidget32': (context) => GeneratedPerfilWidget32(),
+        '/GeneratedMenuinferiorWidget16': (context) =>
+            GeneratedMenuinferiorWidget16(),
+        '/GeneratedMenusuperiorWidget13': (context) =>
+            GeneratedMenusuperiorWidget13(),
+        '/GeneratedAjustesWidget38': (context) => GeneratedAjustesWidget38(),
+        '/GeneratedLoginWidget3': (context) => GeneratedLoginWidget3(),
+        '/GeneratedSettingssharpWidget3': (context) =>
+            GeneratedSettingssharpWidget3(),
+        '/GeneratedInicioWidget21': (context) => GeneratedInicioWidget21(),
+        '/GeneratedCrearpropiedadWidget3': (context) =>
+            GeneratedCrearpropiedadWidget3(),
+        '/GeneratedFavoritosWidget3': (context) => GeneratedFavoritosWidget3(),
+        '/GeneratedPerfilWidget45': (context) => GeneratedPerfilWidget45(),
+        '/GeneratedMenuinferiorWidget22': (context) =>
+            GeneratedMenuinferiorWidget22(),
+        '/GeneratedMenusuperiorWidget18': (context) =>
+            GeneratedMenusuperiorWidget18(),
+        '/GeneratedAjustesWidget52': (context) => GeneratedAjustesWidget52(),
+        '/GeneratedComponent3Widget': (context) => GeneratedComponent3Widget(),
+        '/GeneratedMenuinferiorWidget24': (context) =>
+            GeneratedMenuinferiorWidget24(),
+        '/GeneratedInputWidget8': (context) => GeneratedInputWidget8(),
+        '/GeneratedBotonWidget5': (context) => GeneratedBotonWidget5(),
+        '/GeneratedBuscadorWidget4': (context) => GeneratedBuscadorWidget4(),
+        '/GeneratedMenusuperiorWidget20': (context) =>
+            GeneratedMenusuperiorWidget20(),
+        '/GeneratedCFE21Widget4': (context) => GeneratedCFE21Widget4(),
+        '/GeneratedLoginWidget4': (context) => GeneratedLoginWidget4(),
+        '/GeneratedBOTONWidget6': (context) => GeneratedBOTONWidget6(),
+        '/GeneratedBotonWidget6': (context) => GeneratedBotonWidget6(),
+        '/GeneratedSettingssharpWidget4': (context) =>
+            GeneratedSettingssharpWidget4(),
+        '/GeneratedInicioWidget29': (context) => GeneratedInicioWidget29(),
+        '/GeneratedCrearpropiedadWidget4': (context) =>
+            GeneratedCrearpropiedadWidget4(),
+        '/GeneratedFavoritosWidget4': (context) => GeneratedFavoritosWidget4(),
+        '/GeneratedPerfilWidget60': (context) => GeneratedPerfilWidget60(),
+        '/GeneratedComponent4Widget4': (context) =>
+            GeneratedComponent4Widget4(),
+        '/GeneratedFrame3Widget': (context) => GeneratedFrame3Widget(),
+        '/GeneratedAjustesWidget68': (context) => GeneratedAjustesWidget68(),
+        '/GeneratedLoginWidget5': (context) => GeneratedLoginWidget5(),
+        '/GeneratedSettingssharpWidget5': (context) =>
+            GeneratedSettingssharpWidget5(),
+        '/GeneratedInicioWidget36': (context) => GeneratedInicioWidget36(),
+        '/GeneratedCrearpropiedadWidget5': (context) =>
+            GeneratedCrearpropiedadWidget5(),
+        '/GeneratedFavoritosWidget5': (context) => GeneratedFavoritosWidget5(),
+        '/GeneratedPerfilWidget73': (context) => GeneratedPerfilWidget73(),
+        '/GeneratedFrame4Widget': (context) => GeneratedFrame4Widget(),
+        '/GeneratedAjustesWidget82': (context) => GeneratedAjustesWidget82(),
+        '/GeneratedLoginWidget6': (context) => GeneratedLoginWidget6(),
+        '/GeneratedSettingssharpWidget6': (context) =>
+            GeneratedSettingssharpWidget6(),
+        '/GeneratedInicioWidget43': (context) => GeneratedInicioWidget43(),
+        '/GeneratedCrearpropiedadWidget6': (context) =>
+            GeneratedCrearpropiedadWidget6(),
+        '/GeneratedFavoritosWidget6': (context) => GeneratedFavoritosWidget6(),
+        '/GeneratedNoticia21Widget': (context) => GeneratedNoticia21Widget(),
+        '/GeneratedNoticia31Widget': (context) => GeneratedNoticia31Widget(),
+        '/GeneratedNoticia11Widget': (context) => GeneratedNoticia11Widget(),
+        '/GeneratedPerfilWidget86': (context) => GeneratedPerfilWidget86(),
+        '/GeneratedMenuinferiorWidget41': (context) =>
+            GeneratedMenuinferiorWidget41(),
+        '/GeneratedMenusuperiorWidget34': (context) =>
+            GeneratedMenusuperiorWidget34(),
+        '/GeneratedAjustesWidget96': (context) => GeneratedAjustesWidget96(),
+        '/GeneratedComponent5Widget': (context) => GeneratedComponent5Widget(),
+        '/GeneratedInputWidget15': (context) => GeneratedInputWidget15(),
+        '/GeneratedBotonWidget10': (context) => GeneratedBotonWidget10(),
+        '/GeneratedBuscadorWidget8': (context) => GeneratedBuscadorWidget8(),
+        '/GeneratedMenusuperiorWidget36': (context) =>
+            GeneratedMenusuperiorWidget36(),
+        '/GeneratedCFE22Widget7': (context) => GeneratedCFE22Widget7(),
+      },
+    );
+  }
+}
